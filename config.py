6b481e2064cce099e2a2240ffbbe945ff1f58bb7
carpedm20 <carpedm20@gmail.com>
2013-08-18 01:24:27 +0900

    pip update

--- config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-__author__ = 'carpedm20'
-
-aes_key='\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-sKey = u'' # you need to know sKey for your current account
-duuid = u'' # you need to know duuid for your current account
-user_id = '' # this is not necessary for most of the commands
--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-__author__ = 'carpedm20'
-
-from kakao import *
-
-chatId = 0L
-s = start()
-suc = write(s, chatId, "(하트)")
